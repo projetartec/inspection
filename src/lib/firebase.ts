70f1fb5db83b4ce5f39b5d50dfe53cca983edd01
Gomes Contra Incêndio <comercialgomescontraincendio@gmail.com>
2025-09-20 23:32:41 +0000

    File changes

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -2,12 +2,12 @@ import { initializeApp, getApps, getApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-  projectId: "studio-8357723187-49b45",
-  appId: "1:599561783967:web:846473323f856a147870aa",
-  apiKey: "AIzaSyC7sLA6P5F5kf7uDZAlPq843WPi-IrYQIY",
-  authDomain: "studio-8357723187-49b45.firebaseapp.com",
+  projectId: "studio-8357723187-fe66d",
+  appId: "1:81481237270:web:abd17e791865c651e20637",
+  apiKey: "AIzaSyBapAHUmTH21SRQ3994mGwTdicWCc3SYV8",
+  authDomain: "studio-8357723187-fe66d.firebaseapp.com",
   measurementId: "",
-  messagingSenderId: "599561783967"
+  messagingSenderId: "81481237270"
 };
 
 // Initialize Firebase
