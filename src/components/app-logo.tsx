829895985ad022992ff72cab19f191cf2f9d5c83
Gomes Contra Incêndio <comercialgomescontraincendio@gmail.com>
2025-10-22 11:05:08 +0000

    https://i.imgur.com/4se4p12.png

--- src/components/app-logo.tsx
+++ src/components/app-logo.tsx
@@ -8,7 +8,7 @@ export function AppLogo() {
         Você pode ajustar 'width' e 'height' para o tamanho desejado.
       */}
       <Image
-        src="https://i.imgur.com/rN9k4rV.png" // Caminho para a sua imagem na pasta 'public'
+        src="https://i.imgur.com/4se4p12.png" // Caminho para a sua imagem na pasta 'public'
         alt="Logo da Empresa"
         width={200} // Largura da imagem em pixels
         height={100} // Altura da imagem em pixels
