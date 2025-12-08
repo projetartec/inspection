0d4eed0b2995b25d520f9e3893ae3d45f4bd9c1d
Gomes Contra Incêndio <comercialgomescontraincendio@gmail.com>
2025-12-08 12:07:55 +0000

    The app isn't starting. Please investigate what could be wrong based on

---
+++ next.config.js
@@ -0,0 +1,46 @@
+
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  /* config options here */
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'placehold.co',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'picsum.photos',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'i.imgur.com',
+        port: '',
+        pathname: '/**',
+      },
+    ],
+  },
+  // This is to allow cross-origin requests in development.
+  // The value is a wildcard to allow any origin, which is safe in this
+  // controlled development environment.
+  allowedDevOrigins: ["*"],
+};
+
+module.exports = nextConfig;
